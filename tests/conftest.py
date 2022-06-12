90b635fd634acb31cea9ec69586931c7f7029148
noa shay <noashay@MacBook-Pro-3.local>
2022-06-13 02:37:06 +0300

    conftest fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,18 @@ from flask_login import FlaskLoginClient
 from Tracker import create_app, db
 from Tracker.config import TestConfig
 from Tracker.models import Category, User
+from pathlib import Path
+
+@pytest.fixture(scope='session')
+def test_app():
+    flask_app = create_app(config_class=TestConfig)
+    yield flask_app
 
 
 @pytest.fixture(scope='module')
-def test_client():
-    flask_app = create_app(config_class=TestConfig)
-    with flask_app.test_client() as testing_client:
-        with flask_app.app_context():
+def test_client(test_app):
+    with test_app.test_client() as testing_client:
+        with test_app.app_context():
             yield testing_client
 
 
@@ -43,16 +48,13 @@ def init_database(test_client, new_user):
     db.session.add(new_user)
     db.session.commit()
     yield db
-
     db.drop_all()
 
 
 @pytest.fixture(scope='module')
-def logged_in_user(new_user):
-    flask_app = create_app(config_class=TestConfig)
-
-    with flask_app.app_context():
-        db.create_all(app=flask_app)
+def logged_in_user(new_user, test_app):
+    with test_app.app_context():
+        db.create_all(app=test_app)
         db.session.add(new_user)
         db.session.commit()
 
@@ -62,8 +64,9 @@ def logged_in_user(new_user):
             db.session.add(category)
         db.session.commit()
        
-        flask_app.test_client_class = FlaskLoginClient
-        with flask_app.test_client(user=new_user) as testing_client:
+        test_app.test_client_class = FlaskLoginClient
+        with test_app.test_client(user=new_user) as testing_client:
             yield testing_client
       
         db.drop_all()
+
